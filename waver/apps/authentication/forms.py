eb03b90faed153a3a83726ba3dbde7d7081bfb38
sqlwsxo <stasik.buberenko@gmail.com>
2024-10-10 19:10:02 +0300

    some

--- waver/apps/authentication/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-from django.contrib.auth.forms import AuthenticationForm
-
-class CustomAuthenticationForm(AuthenticationForm):
-   username = forms.CharField(label='Username', max_length=100)
-   password = forms.CharField(label='Password', widget=forms.PasswordInput)
