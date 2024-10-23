005b8f253efacb0c1815ec9bf9457415fb19663c
allr1ght <stasik.buberenko@gmail.com>
2024-10-23 19:42:06 +0300

    update

--- waver/waver/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('auth/', include('apps.authentication.urls')),  # Аутентификация
-    path('messages/', include('apps.messaging.urls')),   # Сообщения
-    path('groups/', include('apps.groups.urls')),        # Группы
-]
